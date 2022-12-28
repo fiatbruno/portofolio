c4ad858ab66366667ef9a9fe9db7adfcd81915f8
Fiat Bruno <fiatbruno10@gmail.com>
2022-12-28 16:54:31 +0200

    Edited about me message

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,10 @@ const About = () => {
         <section className='about'>
             <h2>About Me</h2>
             <p>
-                18. Developing smart contracts in Solidity
-                
+                18. Developing smart contracts in Solidity.
+                Making real world solutions to everyday problems using bockchain technology.
+
+                On the side, I work on Inzoo Media, we're helping real estate agents do what they do best.
             </p>
         </section>
     );
